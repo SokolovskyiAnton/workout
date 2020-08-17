5d74b26b2c918b9fc2bc06216bbafd21bbc5f809
Your Name <you@example.com>
2020-08-17 14:48:51 +0300

    Initial dist subtree commit

---
+++ precache-manifest.a41baa912ee86b53df1685398e43d2d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b72b21103f248b38f97",
+    "url": "/workout/css/app.2c5f6b0f.css"
+  },
+  {
+    "revision": "0178047eee7b8e397b7b",
+    "url": "/workout/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "81c849e4ebf46a433b28d0b5c1b62ba7",
+    "url": "/workout/index.html"
+  },
+  {
+    "revision": "4b72b21103f248b38f97",
+    "url": "/workout/js/app.1e86a571.js"
+  },
+  {
+    "revision": "0178047eee7b8e397b7b",
+    "url": "/workout/js/chunk-vendors.31e690f4.js"
+  },
+  {
+    "revision": "a02c50ecc6731b84aacd624f557789d5",
+    "url": "/workout/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/workout/robots.txt"
+  }
+]);
